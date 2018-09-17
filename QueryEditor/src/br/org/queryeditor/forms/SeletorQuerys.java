cfc91522276c182804eb9030e1e84530c067f5e8
Unknown <israelgomes05@gmail.com>
2018-09-17 00:01:42 -0400

    Adicionado seletor de Querys

---
+++ QueryEditor/src/br/org/queryeditor/forms/SeletorQuerys.java
@@ -0,0 +1,192 @@
+package br.org.queryeditor.forms;
+
+import java.util.HashMap;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * Função<br>
+ *
+ * created 00/00/2018<br>
+ * lastModified 00/00/2018
+ *
+ * @author Israel Gomes
+ * @version 1.0
+ * @since 1.0
+ */
+public class SeletorQuerys extends javax.swing.JDialog {
+
+    private HashMap<String, String> querys;
+    private final DefaultTableModel tabela;
+    private String querySelecionada = "";
+
+    public SeletorQuerys(java.awt.Frame parent, boolean modal, HashMap<String, String> querys) {
+        super(parent, modal);
+        initComponents();
+        tabela = (DefaultTableModel) jtbQuerys.getModel();
+        this.querys = querys;
+        this.preencherTabela(querys);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        btnOk = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jtbQuerys = new javax.swing.JTable();
+        btnCancelar = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setResizable(false);
+
+        btnOk.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        btnOk.setText("OK");
+        btnOk.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnOkActionPerformed(evt);
+            }
+        });
+
+        jtbQuerys.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        jtbQuerys.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Descrição"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jtbQuerys.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jtbQuerysMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(jtbQuerys);
+        if (jtbQuerys.getColumnModel().getColumnCount() > 0) {
+            jtbQuerys.getColumnModel().getColumn(0).setPreferredWidth(300);
+        }
+
+        btnCancelar.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        btnCancelar.setText("CANCELAR");
+        btnCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCancelarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btnCancelar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 183, Short.MAX_VALUE)
+                .addComponent(btnOk)
+                .addContainerGap())
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 403, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnOk)
+                    .addComponent(btnCancelar))
+                .addContainerGap())
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnOkActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOkActionPerformed
+        this.selecionarQuery();
+    }//GEN-LAST:event_btnOkActionPerformed
+
+    private void jtbQuerysMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jtbQuerysMouseClicked
+        if (evt.getClickCount() == 2) {
+            this.selecionarQuery();
+        }
+    }//GEN-LAST:event_jtbQuerysMouseClicked
+
+    private void btnCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelarActionPerformed
+        this.querySelecionada = "";
+        this.dispose();
+    }//GEN-LAST:event_btnCancelarActionPerformed
+
+    public String getQuerySelecionada() {
+        return querySelecionada;
+    }
+
+    private void selecionarQuery() {
+        if (jtbQuerys.getSelectedRow() != -1) {
+            String chave = (String) tabela.getValueAt(jtbQuerys.getSelectedRow(), 0);
+            this.querySelecionada = this.querys.get(chave);
+        }
+        this.dispose();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(SeletorQuerys.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(SeletorQuerys.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(SeletorQuerys.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(SeletorQuerys.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCancelar;
+    private javax.swing.JButton btnOk;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jtbQuerys;
+    // End of variables declaration//GEN-END:variables
+
+    private void preencherTabela(HashMap<String, String> querys) {
+        for (String value : querys.keySet()) {
+            tabela.addRow(new Object[]{value});
+        }
+    }
+}
